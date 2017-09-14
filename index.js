f09136aa3520bf3b783c14787997fbdd1f002a22
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-14 03:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ function moveDodger(e) {
    */
    if(e.which === LEFT_ARROW) {
      moveDodgerLeft();
-     e.preventDefault();
+    e.stopPropagation();
    } else if (e.which === RIGHT_ARROW) {
      moveDodgerRight();
      e.preventDefault();
