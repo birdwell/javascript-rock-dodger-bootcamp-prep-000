850ef71e8f1b4a82ce2059d6fccba28c5e2438e4
Josh Birdwell <jbirdwell34@gmail.com>
2017-09-14 02:55:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -161,8 +161,8 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    window.requestAnimationFrame(() => {		
-     const right = positionToInteger(DODGER.style.right);
-     if (right > 400) DODGER.style.right = `${right - 4}px`;
+     const left = positionToInteger(DODGER.style.left);
+     if (left < 360) DODGER.style.left = `${left - 4}px`;
    });  
 }
 
